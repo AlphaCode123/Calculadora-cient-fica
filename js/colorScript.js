a90d2bae1f33b2c6ab58e58238e50c53a96be1eb
Marcos Daniel da Silva Lima <marcos.d.s.lima1408@gmail.com>
2024-11-07 13:36:32 -0300

    docs. add CalcBasica.html, colorScript.js: Adicionei comentários aos códigos

--- js/colorScript.js
+++ js/colorScript.js
@@ -1,25 +1,30 @@
 //Variaveis globais
 let page = document.querySelector("html");
 
+//Função que ao iniciar o site aplica a coloração padrão
 function startPage(){
     page.classList.add("defaultMode")
 }
 
+//Função que aplica a coloração padrão
 function defaultMode(){
     page.classList.remove("darkMode", "colorBlindMode");
     page.classList.add("defaultMode");
 }
 
+//Função que aplica a coloração escura
 function darkMode(){
     page.classList.remove("defaultMode", "colorBlindMode");
     page.classList.add("darkMode");
 }
 
+//Função que aplica a coloração adaptada para daltônicos
 function colorBlindMode(){
     page.classList.remove("defaultMode", "darkMode");
     page.classList.add("colorBlindMode");
 }
 
+//Função que detecta quando o site é iniciado e chama a função de aplicar cores
 window.onload = function(){
     startPage();
 }
